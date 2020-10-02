88a8f65e1ba677c48f72a6137ab14dcaeeb6d388
BlackFxTalon <andyv@inbox.ru>
2020-10-02 15:38:22 +0500

    page 2 done

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const hambBtn = document.querySelector('.hamb-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+hambBtn.addEventListener('click', function () {
+    this.classList.add('hamb-btn--opened');
+})
+
+closeBtn.addEventListener('click', function () {
+    hambBtn.classList.remove('hamb-btn--opened');
+})
