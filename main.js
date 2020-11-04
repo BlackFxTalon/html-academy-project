dd0a6e2f9e71524db200453ac4ae5cf39c33adfe
BlackFxTalon <andyv@inbox.ru>
2020-11-04 15:36:03 +0500

    добавил подгрузку карточек статей, блогов через кнопкупоказать еще

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 import mobileMenu from './mobileMenu.js';
+
 // main.js scripts
 
 const siteInputs = document.querySelectorAll('input');
 const siteTextareas = document.querySelectorAll('textarea');
 const btnSubmit = document.querySelector('.btn--submit');
+
+const btnShowMoreArticles = document.querySelector('.btn--show-more-articles');
+const btnShowMoreBlogs = document.querySelector('.btn--show-more-blogs');
+const loader = document.querySelector('.loader-default');
+
+
 
 siteInputs.forEach(element => {
     element.addEventListener('input', () => {
@@ -31,4 +38,70 @@ siteTextareas.forEach(element => {
 
 mobileMenu();
 
+function loadMoreArticles() {
+    fetch("articleCards.html", {
+        method: 'GET',
+    })
+    .then(response =>{
+       return response.text();
+    })
+    .then(data => {
+        // Convert the HTML string into a document object
+        // DOMParser() is native method
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
 
+        const articleDoc = doc.querySelector('.row--articles');
+
+        btnShowMoreArticles.disabled = true;
+        loader.classList.add('is-active');
+
+        setTimeout(()=>{
+            loader.classList.remove('is-active');
+            btnShowMoreArticles.disabled = false;
+            btnShowMoreArticles.parentNode.insertBefore(articleDoc, btnShowMoreArticles);
+        }, 1000);
+
+    }).catch(error => {
+        error = alert('error!');
+        loader.classList.remove('is-active');
+        btnShowMoreArticles.disabled = false;
+      });
+}
+
+function loadMoreBlogs() {
+    fetch("blogCards.html", {
+        method: 'GET',
+    })
+    .then(response =>{
+       return response.text();
+    })
+    .then(data => {
+        // Convert the HTML string into a document object
+        // DOMParser() is native method
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
+
+        const blogDoc = doc.querySelector('.row--portfolio-cards');
+
+        btnShowMoreBlogs.disabled = true;
+        loader.classList.add('is-active');
+
+        setTimeout(()=>{
+            loader.classList.remove('is-active');
+            btnShowMoreBlogs.disabled = false;
+            btnShowMoreBlogs.parentNode.insertBefore(blogDoc, btnShowMoreBlogs);
+        }, 1000);
+
+    }).catch(error => {
+        error = alert('error!');
+        loader.classList.remove('is-active');
+        btnShowMoreBlogs.disabled = false;
+      });
+}
+
+
+
+
+  btnShowMoreArticles.addEventListener('click', loadMoreArticles);
+  btnShowMoreBlogs.addEventListener('click', loadMoreBlogs);
