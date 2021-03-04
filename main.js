e06e1e14c04e256a53d7044b36e8bda83b7bb852
blackfxtalon <andyv@inbox.ru>
2021-03-04 15:09:03 +0500

    update

--- main.js
+++ main.js
@@ -1,107 +1,137 @@
-import mobileMenu from './mobileMenu.js';
+import mobileMenu from "./mobileMenu.js";
+mobileMenu();
 
 // main.js scripts
 
-const siteInputs = document.querySelectorAll('input');
-const siteTextareas = document.querySelectorAll('textarea');
-const btnSubmit = document.querySelector('.btn--submit');
+const siteInputs = document.querySelectorAll("input");
+const siteTextareas = document.querySelectorAll("textarea");
+const btnSubmit = document.querySelector(".btn--submit");
 
-const btnShowMoreArticles = document.querySelector('.btn--show-more-articles');
-const btnShowMoreBlogs = document.querySelector('.btn--show-more-blogs');
-const loader = document.querySelector('.loader-default');
+const btnShowMoreArticles = document.querySelector(".btn--show-more-articles");
+const btnShowMoreBlogs = document.querySelector(".btn--show-more-blogs");
+const loader = document.querySelector(".loader-default");
 
+// const filterBtn = document.querySelector(".btn__filter-btn");
+// const articleCards = document.querySelectorAll(".article-card");
+// show/hide article cards based on selected option with data-attributes
 
+// filterBtn.addEventListener("change", function (e) {
+//     articleCards.forEach((elements) => {
+//         const journeyArticleCards = elements.dataset.tag === "journey";
+//         const cityArticleCards = elements.dataset.city;
+//         const booksArticleCards = elements.dataset.books;
+//         const photosArticleCards = elements.dataset.photos;
+//         const receiptsArticleCards = elements.dataset.receipts;
 
-siteInputs.forEach(element => {
-    element.addEventListener('input', () => {
-        if(element.value.length < 10 || element.value.length > 200) {
-            element.classList.add('error');
+//         if (e.target.value === "" || e.target.value === "all articles") {
+//             elements.classList.add("d-flex");
+//         } else if (e.target.value === journeyArticleCards) {
+//             journeyArticleCards.classList.add("d-none");
+//         } else if (e.target.value === cityArticleCards) {
+//             cityArticleCards.classList.add("d-none");
+//         } else if (e.target.value === booksArticleCards) {
+//             booksArticleCards.classList.add("d-none");
+//         } else if (e.target.value === photosArticleCards) {
+//             photosArticleCards.classList.add("d-none");
+//         } else if (e.target.value === receiptsArticleCards) {
+//             receiptsArticleCards.classList.add("d-none");
+//         }
+//     });
+// });
+
+// simple input scripts validation
+siteInputs.forEach((element) => {
+    element.addEventListener("input", () => {
+        if (element.value.length < 10 || element.value.length > 200) {
+            element.classList.add("error");
             btnSubmit.disabled = true;
         } else {
-            element.classList.remove('error');
+            element.classList.remove("error");
             btnSubmit.disabled = false;
         }
-    })
+    });
 });
 
-siteTextareas.forEach(element => {
-    element.addEventListener('input', () => {
-        if(element.value.length < 10 || element.value.length > 200) {
-            element.classList.add('error');
+siteTextareas.forEach((element) => {
+    element.addEventListener("input", () => {
+        if (element.value.length < 10 || element.value.length > 200) {
+            element.classList.add("error");
             btnSubmit.disabled = true;
         } else {
-            element.classList.remove('error');
+            element.classList.remove("error");
             btnSubmit.disabled = false;
         }
-    })
+    });
 });
 
-mobileMenu();
-
+// ajax scripts for articles, blogs
 function loadMoreArticles() {
     fetch("articleCards.html", {
-        method: 'GET',
+        method: "GET",
     })
-    .then(response =>{
-       return response.text();
-    })
-    .then(data => {
-        // Convert the HTML string into a document object
-        // DOMParser() is native method
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(data, 'text/html');
+        .then((response) => {
+            return response.text();
+        })
+        .then((data) => {
+            // Convert the HTML string into a document object
+            // DOMParser() is native method
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(data, "text/html");
 
-        const articleDoc = doc.querySelector('.row--articles');
+            const articleDoc = doc.querySelector(".row--articles");
 
-        btnShowMoreArticles.disabled = true;
-        loader.classList.add('is-active');
+            btnShowMoreArticles.disabled = true;
+            loader.classList.add("is-active");
 
-        setTimeout(()=>{
-            loader.classList.remove('is-active');
+            setTimeout(() => {
+                loader.classList.remove("is-active");
+                btnShowMoreArticles.disabled = false;
+                btnShowMoreArticles.parentNode.insertBefore(
+                    articleDoc,
+                    btnShowMoreArticles
+                );
+            }, 1000);
+        })
+        .catch((error) => {
+            error = alert("error!");
+            loader.classList.remove("is-active");
             btnShowMoreArticles.disabled = false;
-            btnShowMoreArticles.parentNode.insertBefore(articleDoc, btnShowMoreArticles);
-        }, 1000);
-
-    }).catch(error => {
-        error = alert('error!');
-        loader.classList.remove('is-active');
-        btnShowMoreArticles.disabled = false;
-      });
+        });
 }
 
 function loadMoreBlogs() {
     fetch("blogCards.html", {
-        method: 'GET',
+        method: "GET",
     })
-    .then(response =>{
-       return response.text();
-    })
-    .then(data => {
-        // Convert the HTML string into a document object
-        // DOMParser() is native method
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(data, 'text/html');
+        .then((response) => {
+            return response.text();
+        })
+        .then((data) => {
+            // Convert the HTML string into a document object
+            // DOMParser() is native method
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(data, "text/html");
 
-        const blogDoc = doc.querySelector('.row--portfolio-cards');
+            const blogDoc = doc.querySelector(".row--portfolio-cards");
 
-        btnShowMoreBlogs.disabled = true;
-        loader.classList.add('is-active');
+            btnShowMoreBlogs.disabled = true;
+            loader.classList.add("is-active");
 
-        setTimeout(()=>{
-            loader.classList.remove('is-active');
+            setTimeout(() => {
+                loader.classList.remove("is-active");
+                btnShowMoreBlogs.disabled = false;
+                btnShowMoreBlogs.parentNode.insertBefore(
+                    blogDoc,
+                    btnShowMoreBlogs
+                );
+            }, 1000);
+        })
+        .catch((error) => {
+            error = alert("error!");
+            loader.classList.remove("is-active");
             btnShowMoreBlogs.disabled = false;
-            btnShowMoreBlogs.parentNode.insertBefore(blogDoc, btnShowMoreBlogs);
-        }, 1000);
-
-    }).catch(error => {
-        error = alert('error!');
-        loader.classList.remove('is-active');
-        btnShowMoreBlogs.disabled = false;
-      });
+        });
 }
 
-
-
-
-  btnShowMoreArticles.addEventListener('click', loadMoreArticles);
-  btnShowMoreBlogs.addEventListener('click', loadMoreBlogs);
+btnShowMoreArticles.addEventListener("click", loadMoreArticles);
+btnShowMoreBlogs.addEventListener("click", loadMoreBlogs);
